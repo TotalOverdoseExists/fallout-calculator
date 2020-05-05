324a173eeaccf63d2ce59d9faa4bc98088433a65
TotalOverdoseExists <verylonghair00@gmail.com>
2020-05-05 18:11:41 +0500

    start

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react'
 import './css/reset.css'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function LanguageSelect(props) {
+	return (
+		<button>
+			{props.lang}
+		</button>
+	)
+}
+
+class App extends React.Component {
+	render() {
+		return (
+			<header>
+				<h1>Fallout Calculator</h1>
+				<div className='m-language'>
+					<LanguageSelect lang='en' />
+					<LanguageSelect lang='ru' />
+				</div>
+			</header>
+		)
+	}
 }
 
 export default App
