63cc845bb945b02dcfca3586e939d9f18384bf45
TotalOverdoseExists <verylonghair00@gmail.com>
2020-06-18 18:26:09 +0500

    renew

--- src/App.js
+++ src/App.js
@@ -2,27 +2,49 @@ import React from 'react'
 import './css/reset.css'
 import './App.css'
 
-function LanguageSelect(props) {
-	return (
-		<button>
-			{props.lang}
-		</button>
-	)
-}
-
 class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			lang: '',
+			name: 'Name',
+			age: 18,
+			sex: 'Male',
+			race: 'Human'
+		}
+	}
+
 	render() {
 		return (
-			<header>
-				<h1>Fallout Calculator</h1>
-				<div className='m-language'>
-					<LanguageSelect lang='en' />
-					<LanguageSelect lang='ru' />
-				</div>
-			</header>
-			<main>
-				
-			</main>
+			<section>
+				<header>
+					<h1>Fallout Calculator</h1>
+					<div className='m-language'>
+						<button>{this.state.lang = 'en'}</button>
+						<button>{this.state.lang = 'ru'}</button>
+					</div>
+				</header>
+				<main>
+					<div className='m-person'>
+						<button>
+							{this.state.name}
+						</button>
+						<button>
+							{this.state.age}
+						</button>
+						<button>
+							{this.state.sex}
+						</button>
+						<button>
+							{this.state.race}
+						</button>
+					</div>
+					<div className='m-characteristics'></div>
+					<div className='m-traits'></div>
+					<div className='m-skills'></div>
+					<div className='m-view'></div>
+				</main>
+			</section>
 		)
 	}
 }
